f0bc825a0bc9f8212ea33f9af0dae40b4da9505b
Shristi Pandey <goldiep04@gmail.com>
2024-05-24 22:44:29 +0530

    update the index and dashboard

--- client/src/pages/admin/dashboard.jsx
+++ client/src/pages/admin/dashboard.jsx
@@ -1,29 +1,59 @@
-import React from 'react'
-import AdminLayout from '../../components/Layout/AdminLayout'
-import { Box, Container, Paper, Stack, Typography } from '@mui/material'
-import { AdminPanelSettings as AdminPanelSettingsIcon,
-     Group as GroupIcon, Message as MessageIcon, Notifications as NotificationsIcon,
-      Person  as PersonIcon} from '@mui/icons-material'
-import moment from 'moment'
-import { CurveButton, SearchField } from '../../components/styles/StyledComponents'
-import { matBlack } from '../../constants/color'
- import { DoughnutChart, LineChart } from "../../components/specific/Charts";
-
-
+// import { useFetchData } from "6pp";
+import {
+  AdminPanelSettings as AdminPanelSettingsIcon,
+  Group as GroupIcon,
+  Message as MessageIcon,
+  Notifications as NotificationsIcon,
+  Person as PersonIcon,
+} from "@mui/icons-material";
+import {
+  Box,
+  Container,
+  Paper,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import moment from "moment";
+import React from "react";
+import AdminLayout from "../../components/Layout/AdminLayout";
+import { DoughnutChart, LineChart } from "../../components/specific/Charts";
+import {
+  CurveButton,
+  SearchField,
+} from "../../components/styles/StyledComponents";
+import { matBlack } from "../../constants/color";
+// import { server } from "../../constants/config";
+// import { useErrors } from "../../hooks/hook";
 
 const Dashboard = () => {
+  // const { loading, data, error } = useFetchData(
+  //   `${server}/api/v1/admin/stats`,
+  //   "dashboard-stats"
+  // );
 
-    const Appbar=( 
-    <Paper  elevation={3}
-    sx={{padding:"2rem", margin:"2rem 0",borderRadius:"1rem"}}
+  // const { stats } = data || {};
+
+  // useErrors([
+  //   {
+  //     isError: error,
+  //     error: error,
+  //   },
+  // ]);
+
+  const Appbar = (
+    <Paper
+      elevation={3}
+      sx={{ padding: "2rem", margin: "2rem 0", borderRadius: "1rem" }}
     >
-        <Stack  direction={"row"} alignItems={"center"} spacing={"1rem"} >
-            <AdminPanelSettingsIcon sx={{fontSize:"3rem"}}  /> 
-            <SearchField  placeholder='Search...' />
-            <CurveButton>Search</CurveButton>
+      <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
+        <AdminPanelSettingsIcon sx={{ fontSize: "3rem" }} />
 
-            <Box  flexGrow={1} />
-            <Typography
+        <SearchField placeholder="Search..." />
+
+        <CurveButton>Search</CurveButton>
+        <Box flexGrow={1} />
+        <Typography
           display={{
             xs: "none",
             lg: "block",
@@ -33,51 +63,76 @@ const Dashboard = () => {
         >
           {moment().format("dddd, D MMMM YYYY")}
         </Typography>
-          <NotificationsIcon />
-        </Stack>
+
+        <NotificationsIcon />
+      </Stack>
     </Paper>
-    )
+  );
 
-    const Widgets =(
-        <Stack   
-        direction={{
-            xs:"column", sm:"row"
-        }}
-        justifyContent="space-between"
-        alignItems={"center"}
-        margin={"2rem 0"}
-
-        >
-            <Widget title={"Users"} value={34} Icon={<PersonIcon/>} />
-            <Widget title={"Chats"} value={3}  Icon={<GroupIcon/>} />
-            <Widget title={"Messages"} value={453} Icon={<MessageIcon/>}  />
-        </Stack>
-    )
-    
+  const Widgets = (
+    <Stack
+      direction={{
+        xs: "column",
+        sm: "row",
+      }}
+      spacing="2rem"
+      justifyContent="space-between"
+      alignItems={"center"}
+      margin={"2rem 0"}
+    >
+      <Widget title={"Users"} value={34} Icon={<PersonIcon />} />
+      <Widget
+        title={"Chats"}
+         value={3}
+        Icon={<GroupIcon />}
+      />
+      <Widget
+        title={"Messages"}
+         value={453}
+        Icon={<MessageIcon />}
+      />
+    </Stack>
+  );
 
   return (
     <AdminLayout>
-        <Container  component={"main"} >
-            {Appbar}
-            
-            <Stack direction={{xs:"column",lg:"row"}} spacing={"2rem"}
-             flexWrap={"wrap"}  justifyContent={"center"}
-             alignItems={{
-              xs:"center",
-              lg:"stretch"
-             }}
-             sx={{gap:"2rem"}}
-             >
-                <Paper   
-                 elevation={3}
-    sx={{padding:"2rem 3.5rem", borderRadius:"1rem",width:"100%", maxWidth:"45rem"}}
-                >
-                    <Typography >Last Messages</Typography>
-                   <LineChart  value={[23,56,33,67]} />
-                    
-                </Paper >
-                <Paper 
-                 elevation={3}
+      {/* {loading ? (
+        <Skeleton height={"100vh"} />
+      ) : ( */}
+        <Container component={"main"}>
+          {Appbar}
+
+          <Stack
+            direction={{
+              xs: "column",
+              lg: "row",
+            }}
+            flexWrap={"wrap"}
+            justifyContent={"center"}
+            alignItems={{
+              xs: "center",
+              lg: "stretch",
+            }}
+            sx={{ gap: "2rem" }}
+          >
+            <Paper
+              elevation={3}
+              sx={{
+                padding: "2rem 3.5rem",
+                borderRadius: "1rem",
+                width: "100%",
+                maxWidth: "45rem",
+              }}
+            >
+              <Typography margin={"2rem 0"} variant="h4">
+                Last Messages
+              </Typography>
+
+              <LineChart value={[23,56,33,67]} />
+            </Paper>
+
+            <Paper
+              elevation={3}
               sx={{
                 padding: "1rem ",
                 borderRadius: "1rem",
@@ -86,49 +141,47 @@ const Dashboard = () => {
                 alignItems: "center",
                 width: { xs: "100%", sm: "50%" },
                 position: "relative",
-                
                 maxWidth: "25rem",
               }}
-                >
-                  <DoughnutChart labels={["Single Chats","Group Chats"]} value={[23,66]} />
+            >
+              <DoughnutChart
+                labels={["Single Chats","Group Chats"]} value={[23,66]}
+              />
 
-                   <Stack  
-                    position={"absolute"}
+              <Stack
+                position={"absolute"}
                 direction={"row"}
                 justifyContent={"center"}
                 alignItems={"center"}
                 spacing={"0.5rem"}
                 width={"100%"}
                 height={"100%"}
-                   >
-            <GroupIcon /><Typography>Vs</Typography>
-            <PersonIcon />
-                   </Stack>
+              >
+                <GroupIcon /> <Typography>Vs </Typography>
+                <PersonIcon />
+              </Stack>
+            </Paper>
+          </Stack>
 
-                </Paper>
-            </Stack>
-            {
-                Widgets
-            }
-            </Container>
+          {Widgets}
+        </Container>
+      {/* )} */}
     </AdminLayout>
-  )
+  );
 };
 
-const Widget=({title,value,Icon})=>(
-
-<Paper
-        elevation={3}
+const Widget = ({ title, value, Icon }) => (
+  <Paper
+    elevation={3}
     sx={{
       padding: "2rem",
       margin: "2rem 0",
       borderRadius: "1.5rem",
       width: "20rem",
     }}
-
-   >
-    <Stack   alignItems={"center"} spacing={"1rem"}> 
-        <Typography
+  >
+    <Stack alignItems={"center"} spacing={"1rem"}>
+      <Typography
         sx={{
           color: "rgba(0,0,0,0.7)",
           borderRadius: "50%",
@@ -139,16 +192,15 @@ const Widget=({title,value,Icon})=>(
           justifyContent: "center",
           alignItems: "center",
         }}
-
-        >{value}</Typography>
-    <Stack  direction={"row"} spacing={"1rem"} alignItems={"center"} >
-
-    {Icon}
-    <Typography>{title}</Typography>
+      >
+        {value}
+      </Typography>
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        {Icon}
+        <Typography>{title}</Typography>
+      </Stack>
     </Stack>
-    </Stack>
-    
-    </Paper>
+  </Paper>
 );
 
-export default Dashboard
+export default Dashboard;
